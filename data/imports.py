fe58adac39da60c53f7bade73116cf89998ac4f7
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-12-20 18:06:18 +0100

    Create imports.py

---
+++ data/imports.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd 
+import seaborn as sns
+import math
+from itertools import combinations
+from itertools import product
+
+# matplotlib imports
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sklearn.manifold import MDS # multidimensional scaling
+from sklearn.decomposition import PCA # principal component analysis
+from scipy import stats # for correlation
+
