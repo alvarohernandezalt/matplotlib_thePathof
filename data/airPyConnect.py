632107dc68b0efd92695402f815c00b809923203
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-12-21 20:52:49 +0100

    Update airPyConnect.py

--- data/airPyConnect.py
+++ data/airPyConnect.py
@@ -19,34 +19,4 @@ def create_dataframe(api_key, base_id, table_name):
 
 df = create_dataframe(api_key, base_id, table_name)
 
-df['Pais'] = ['Spain','Spain', 'Spain']
 
-import requests
-import json
-
-# Define the URL and headers
-url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
-headers = {
-    "Authorization": f"Bearer {api_key}",
-    "Content-Type": "application/json"
-}
-
-# Define the data
-data = {
-    "description": "Number of goals.",
-    "name": "Goles",
-    "options": {
-      "color": "greenBright",
-      "icon": "check"
-    },
-    "type": "checkbox"
-}
-
-# Make the POST request
-response = requests.post(url, headers=headers, data=json.dumps(data))
-
-# Check the response
-if response.status_code == 200:
-    print("Field created successfully.")
-else:
-    print("Failed to create field. Response:", response.content)
