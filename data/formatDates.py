c6bacd1ec80b78c843a173cba97257f30069ba8c
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2024-01-03 17:06:28 +0100

    Update formatDates.py

--- data/formatDates.py
+++ data/formatDates.py
@@ -1,24 +1,41 @@
-import matplotlib.dates as mdates
+import pandas as pd
+import matplotlib.pyplot as plt
 
-fig, ax = plt.figure(figsize = (24, 6), facecolor='#9F9F9F'), plt.axes(facecolor='#9F9F9F')
+# Let's use some timebased data
+url = "https://raw.githubusercontent.com/alvarohernandezalt/matplotlib_thePathof/main/data/hrp_state.csv"
+df = pd.read_csv(url)
+df
+
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+
+fig, ax = plt.figure(figsize = (12, 6), facecolor='#9F9F9F'), plt.axes(facecolor='#9F9F9F')
 
 # Change the params of both ticks and ticklegend
 ax.tick_params(axis='x', colors='#C6C8CB')
 ax.tick_params(axis='y', colors='#C6C8CB')
 
 # Define formatter for date
-xformatter = mdates.DateFormatter('%d-%m')
+xformatter = mdates.DateFormatter('%d-%m-%y')
+ax.xaxis.set_major_locator(mdates.DayLocator(bymonthday=[1]))
+
+# Define a custom formatter function
+def custom_formatter(x, pos):
+    return format(int(x), ',').replace(',', '.')
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(custom_formatter))
+
 
 # Change the color of spines
 for spine in 'bottom', 'left', 'top', 'right':
   ax.spines[spine].set_color('#C6C8CB')
 
 
-df = df_clean.drop(columns=['ESTADO_SEGUIMIENTO','cambiadoSeguimientoLast', 'OP_NOMBRE', 'TIPOLOGIA'])
+
 df['Importe_MAX'] = df['Importe_MAX'].replace({'€': '', ',': '.'}, regex=True).astype(float)
 
-ax.plot(df.Created, df.Importe_MAX);
-
+df.set_index('fecha_RRR').plot(ax=ax)
 ax.legend().set_visible(False)
-ax.set_xlabel('Created Time', color='#C6C8CB', fontproperties=font2);
+
+
+ax.set_xlabel('Modified Day', color='#C6C8CB', fontproperties=font2);
 ax.xaxis.set_major_formatter(xformatter)
