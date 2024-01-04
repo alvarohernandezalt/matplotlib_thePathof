43af2904a7bda47b1cc1c1b383d88bc47d7d06c8
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2024-01-04 08:57:41 +0100

    Update wishartRandomPlot.py

--- data/wishartRandomPlot.py
+++ data/wishartRandomPlot.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import wishart, chi2
+from scipy.integrate import quad
+from matplotlib.ticker import FuncFormatter
+
 
 fig, ax = plt.figure(figsize = (12, 6), facecolor='#9F9F9F'), plt.axes(facecolor='#9F9F9F')
 
@@ -15,24 +18,48 @@ for spine in 'bottom', 'left', 'top', 'right':
 ax.set_xlim(0.0,8.0)
 ax.set_ylim(0,0.3)
 
+formatter = FuncFormatter(lambda x, pos: "{:.2f}".format(x))
+ax.xaxis.set_major_formatter(formatter)
+
+scale_matrix = np.array([[1]])
+
 x = np.linspace(1e-5, 8, 100)
-w = wishart.pdf(x, df=3, scale=1); w[:5]
+w = wishart.pdf(x, df=3, scale=scale_matrix); w[:5]
 
 ax.plot(x,w,color='#EBBD2A', linewidth=2)
 ax.set_title('Wishart Random Variable', color='#C6C8CB', fontproperties=font2, fontsize=12)
 
 # Set a point
-z = 3.3
-pdfz= wishart.pdf(z, df=3, scale=1)
+z = 3.30
+pdfz= wishart.pdf(z, df=3, scale=scale_matrix)
 ax.plot([z,z],
        [0,pdfz],
        color='#EB452A',
        linestyle='dashed')
 ax.plot(z,pdfz,color='#EB452A',marker='.',)
 ax.text(z + .1, pdfz,
-        s=f'z = {z}', color='#C6C8CB')
+        s='z = {:.2f}'.format(z), color='#C6C8CB', size = 11)
 
-# New code to fill the area
-x_fill = np.linspace(1e-5, z, 100)
-y_fill = wishart.pdf(x_fill, df=3, scale=1)
-ax.fill_between(x_fill, 0, y_fill, color='#EBA62A', alpha=0.5)
+# Fill the area
+x_vals = np.linspace(1e-5, z, 100)
+pdfx_vals = wishart.pdf(x_vals, df=3, scale=scale_matrix); w[:5]
+ax.fill_between(x_vals,
+                np.zeros(100),
+                pdfx_vals,
+                color='#EBA62A',
+                alpha=.5)
+
+# Calculate the area and put a text with its value
+def pdf(x):
+  return wishart.pdf(x, df=3, scale=scale_matrix)
+
+area, error = quad(pdf, 1e-5, z)
+
+ax.text(z/2, pdfz/2,
+        s='{:.2f}'.format(area),
+        fontproperties=font2,
+        size = 11,
+        color='#C6C8CB',
+        ha='center');
+
+plt.savefig('WishartRandomVariable.pdf', bbox_inches='tight')
