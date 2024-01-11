7687d2fba7298a0fda7b2d1f68401a4b5e9ef1b3
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2024-01-11 19:07:18 +0100

    Update dataFrameOps.py

--- data/dataFrameOps.py
+++ data/dataFrameOps.py
@@ -2,5 +2,14 @@ import pandas as pd
 
 url = "https://raw.githubusercontent.com/alvarohernandezalt/matplotlib_thePathof/main/data/2015_2023_Budget.csv"
 df = pd.read_csv(url)
-df.head()
+print(df.head())
 
+
+df_year_type = df.groupby('yearBudget')['maxBudget'].sum().reset_index()
+df_year_type.columns = ['uniqueYearBudget', 'total']
+print(df_year_type)
+
+df_year_type = df.groupby(['yearBudget', 'stateBudgetName'])['maxBudget'].sum().reset_index()
+df_year_type = df_year_type.pivot(index='yearBudget', columns='stateBudgetName', values='maxBudget').reset_index()
+df_year_type = df_year_type.fillna(0)
+print(df_year_type)
